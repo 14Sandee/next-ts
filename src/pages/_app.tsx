44e659ed5aa9d94e5981d549fb158c612579cdc9
Sandeep Vishvakarma <sandee.vit14@gmail.com>
2024-05-14 12:13:59 +0530

    feat: added post list compnent

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Roboto } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -17,10 +18,15 @@ const theme = createTheme({
 });
 
 export default function App(props: AppProps) {
+
+  const queryClient = new QueryClient()
   const { Component, pageProps } = props;
+
   return <AppCacheProvider {...props}>
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ThemeProvider>
   </AppCacheProvider>
 }
